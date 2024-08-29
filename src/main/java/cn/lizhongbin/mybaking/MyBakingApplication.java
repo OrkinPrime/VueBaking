84107792ba81d37285d97a6cbc509c92908ebb53
李忠斌 <781195674@qq.com>
2024-08-29 14:24:11 +0800

    完成环境配置

--- src/main/java/cn/lizhongbin/mybaking/MyBakingApplication.java
+++ src/main/java/cn/lizhongbin/mybaking/MyBakingApplication.java
@@ -2,6 +2,7 @@ package cn.lizhongbin.mybaking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class MyBakingApplication {
---
+++ src/main/java/cn/lizhongbin/mybaking/controller/UserController.java
@@ -0,0 +1,4 @@
+package cn.lizhongbin.mybaking.controller;
+
+public class UserController {
+}
